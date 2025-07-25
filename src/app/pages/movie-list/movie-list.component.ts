3ea3ea61e4c763b24acddee57b6ea1ad5865653e
BertrandC44 <courgeau.bertrand@icloud.com>
2025-07-25 14:26:55 +0200

    version 0.2

--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -1,5 +1,8 @@
+declare const UIkit: any;
+
 import { Component } from '@angular/core';
 import {NgStyle} from '@angular/common';
+
 
 @Component({
   selector: 'app-movie-list',
@@ -10,45 +13,23 @@ import {NgStyle} from '@angular/common';
   styleUrl: './movie-list.component.scss'
 })
 export class MovieListComponent {
-  movies = [
-    {
-      id: 1,
-      slug: "1",
-      title: "Inception",
-      year: 2018,
-      author: "Roar Uthaug",
-      duration: 125,
-      genre: "Action, Fantasy",
-      synopsis: "Un voleur s'infiltre dans les rêves pour dérober des secrets.",
-      cover: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
-      rating: 3
-    },
-    {
-      id: 2,
-      slug: "2",
-      title: "The Dark Knight",
-      year: 2018,
-      author: "Roar Uthaug",
-      duration: 125,
-      genre: "Action, Fantasy",
-      synopsis: "Batman affronte le Joker pour sauver Gotham.",
-      cover: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-      rating: 5
-    },
-    {
-      id: 3,
-      slug: "3",
-      title: "Interstellar",
-      year: 2018,
-      author: "Roar Uthaug",
-      duration: 125,
-      genre: "Action, Fantasy",
-      synopsis: "Une équipe voyage à travers un trou de ver pour sauver l'humanité.",
-      cover: "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
-      rating: 4
-    }
-  ];
   stars = [1, 2, 3, 4, 5];
+
+constructor(private movieService: MovieService) {
 }
 
+/**
+ * Sera appelée via le clique d'un bouton
+ */
+callApi() {
+  this.movieService.getMovies().subscribe({
+    next: responseJson => {
+      // je vais récupérer dans tout le json uniquement l'attribut data => la liste des films
+      // rappel le json actuelle => code, message, data (array movies)
+      this.movies = responseJson.data;
+    },
+    error: error => {}
+  });
+}
+}
 
---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class MovieService {
+  constructor(private http: HttpClient) {
+  }
+
+  getMovies() : Observable<ApiResponse<Movie[]>> {
+    return this.http.get<ApiResponse<Movie[]>>("http://localhost:3000/movies");
+  }
+
+
+}
+
+
+export class Movie {
+  public id: bigint;
+  public slug: string;
+  public title: string;
+  public year: bigint;
+  public author: string;
+  public duration: bigint;
+  public genre: string;
+  public synopsis: string;
+  public cover: string;
+  public rating: number;
+
+
+  constructor(id: bigint, slug: string, title: string, year: bigint, author: string, duration: bigint, genre: string, synopsis: string, cover: string, rating: number) {
+    this.id = id;
+    this.slug = slug;
+    this.title = title;
+    this.year = year;
+    this.author = author;
+    this.duration = duration;
+    this.genre = genre;
+    this.synopsis = synopsis;
+    this.cover = cover;
+    this.rating = rating;
+  }
+
+  movies : Movie [] = [];
+}
+
+export class ApiResponse <T> {
+  data: T  | undefined;
+
+
+  constructor(data: T | undefined) {
+    this.data = data;
+  }
+
+
+}
